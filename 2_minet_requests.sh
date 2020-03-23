eda7d44aa9b8462e00435c4f81a530550bb97aa0
heloisethero <thero.heloise@gmail.com>
2020-03-23 17:40:29 +0100

    Solve annoying bash bug about the token format

--- 2_minet_requests.sh
+++ 2_minet_requests.sh
@@ -4,8 +4,8 @@ token_crowdtangle=$(jq '.token_crowdtangle' config.json)
 
 if [ ! -f ./clean_data/fake_posts.csv ]; 
 then
-    minet ct summary url ./clean_data/fake_url.csv --token $token_crowdtangle \
-     --posts ./clean_data/fake_posts.csv --sort-by total_interactions --start-date 2019-01-01
+    bash -c "minet ct summary url ./clean_data/fake_url.csv --token $token_crowdtangle \
+    --posts ./clean_data/fake_posts.csv --sort-by total_interactions --start-date 2019-01-01"
 else
     echo "Nothing to do, the output file already exists"
 fi
