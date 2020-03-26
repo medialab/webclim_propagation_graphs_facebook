0dfe558d84fd0a307875e607abe21476e8434242
heloisethero <thero.heloise@gmail.com>
2020-03-26 18:24:50 +0100

    Clean the article url with ural before using minet to go at the crowdtangle API

--- 2_minet_requests.sh
+++ 2_minet_requests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+minet url-parse --no-strip-protocol url ./clean_data/fake_url.csv > ./clean_data/clean_fake_url.csv
+
 token_crowdtangle=$(jq '.token_crowdtangle' config.json)
 
 if [ ! -f ./clean_data/fake_posts.csv ]; 
 then
-    bash -c "minet ct summary url ./clean_data/fake_url.csv --token $token_crowdtangle \
+    bash -c "minet ct summary normalized_url ./clean_data/clean_fake_url.csv --token $token_crowdtangle \
     --posts ./clean_data/fake_posts.csv --sort-by total_interactions --start-date 2019-01-01"
 else
     echo "Nothing to do, the output file already exists"
