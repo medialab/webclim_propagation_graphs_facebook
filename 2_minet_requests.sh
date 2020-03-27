e211f65af16bf735c896c49557424fcf79a32cd2
heloisethero <thero.heloise@gmail.com>
2020-03-27 11:58:26 +0100

    Resolve properly the annoying bash problem thanks to Guillaume and Benjamin

--- 2_minet_requests.sh
+++ 2_minet_requests.sh
@@ -2,12 +2,12 @@
 
 minet url-parse --no-strip-protocol url ./clean_data/fake_url.csv > ./clean_data/clean_fake_url.csv
 
-token_crowdtangle=$(jq '.token_crowdtangle' config.json)
+token_crowdtangle=$(jq -r '.token_crowdtangle' config.json)
 
 if [ ! -f ./clean_data/fake_posts.csv ]; 
 then
-    bash -c "minet ct summary normalized_url ./clean_data/clean_fake_url.csv --token $token_crowdtangle \
-    --posts ./clean_data/fake_posts.csv --sort-by total_interactions --start-date 2019-01-01"
+    minet ct summary normalized_url ./clean_data/clean_fake_url.csv --token $token_crowdtangle \
+     --posts ./clean_data/fake_posts.csv --sort-by total_interactions --start-date 2019-01-01
 else
     echo "Nothing to do, the output file already exists"
 fi
