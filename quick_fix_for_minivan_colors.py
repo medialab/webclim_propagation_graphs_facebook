d2e63bf448b62e296a5e8aa25057344c5bcffd92
heloisethero <thero.heloise@gmail.com>
2020-04-20 19:43:32 +0200

    more pastel colours for the graphs

--- quick_fix_for_minivan_colors.py
+++ quick_fix_for_minivan_colors.py
@@ -4,20 +4,18 @@ import json
 if __name__ == "__main__":
 
     # choose a scientific topic between: "health", "climate" or "COVID-19":
-    SCIENTIFIC_TOPIC = "COVID-19"
+    SCIENTIFIC_TOPIC = "climate"
     NODE_COLOR = {
-        "climate": "#00F",
-        "health": "#FF0",
-        "COVID-19": "#F00"
+        "climate": "#66F",
+        "health": "#FF9",
+        "COVID-19": "#F66"
         }
 
     with open('./bundle/' + SCIENTIFIC_TOPIC.capitalize() + '.json') as input_file:
         graph = json.load(input_file)
 
     graph["model"]["nodeAttributes"][0]["modalities"]["facebook_account_or_page"]["color"] = NODE_COLOR[SCIENTIFIC_TOPIC]
-    graph["model"]["nodeAttributes"][0]["modalities"]["domain_name"]["color"] = "#000"
-
-    print(json.dumps(graph["model"], indent=4))
+    graph["model"]["nodeAttributes"][0]["modalities"]["domain_name"]["color"] = "#999"
 
     with open('./bundle/' + SCIENTIFIC_TOPIC.capitalize() + '_fixed.json', "w") as output_file:
         json.dump(graph, output_file)
